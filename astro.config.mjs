0caabdf43ff8472d8220981b8efe8ced64c1eaee
Lucas Fernandes <46578395+lafernandes22@users.noreply.github.com>
2026-05-31 21:14:55 -0400

    remove custom CNAME reference

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://comeecalate.com',
+  site: 'https://lafernandes22.github.io',
+  base: '/comeecalate-web',
   output: 'static',
 });
